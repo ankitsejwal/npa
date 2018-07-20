1e4d2cbc9aa9dfbe8d45d4a69009a19386de6fd2
ankitsejwal <ankit@sejw.al>
2018-07-20 13:00:21 +1000

    number to string fnum

--- main.js
+++ main.js
@@ -8,13 +8,14 @@ function calculate() {
     const dollar    = 68;           // dollar to ruppee value
     const crore     = 10000000;
     const million   = 1000000;
-    const billion   = 100000000;
+    const billion   = 1000000000;
 
     let rupee = document.getElementById('rupee-value').value;
     let result = (rupee * crore)/dollar;
     let displayResult = `$ ${Math.round(result)}`
     document.getElementById('result').innerHTML = displayResult;
     console.log(result);
+    console.log(fnum(result));
 }
 
 function enteredValue(){
@@ -23,6 +24,24 @@ function enteredValue(){
     document.getElementById('rs-value-numeric').innerHTML = valueNumeric;
     document.getElementById('rs-value').innerHTML = value;
     // console.log(value);
+}
+
+function fnum(x){
+    if (x < 1000000) {
+        return (Math.round(x/1000) + ' Thousand');
+    }
+    if (x < 10000000) {
+        return (Math.round(x/1000000).toFixed(2) + ' Million');
+    }
+    if (x < 1000000000) {
+        return (Math.round(x/1000000) + ' Million');
+    }
+    if (x < 1000000000000) {
+        return (Math.round(x/1000000000) + ' Billion');
+    }
+    if (x < 1000000000000000) {
+        return (Math.round(x/1000000000000) + ' Trillion');
+    }
 }
 
 // trigger calculate() on ENTER press
